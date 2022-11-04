f7f6944be2f0425d32bcd6e8f31ce3e36444552e
James Yang <jamesyang663@gmail.com>
2022-11-04 04:30:59 -0400

    Added Gradient

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import './form.css';
 
+/**
+ * Colour Bank
+ * #ee7752 - orange
+ * #e73c7e - pink
+ * #23a6d5 - blue
+ * #23d5ab - green
+ */
+
 class Form extends Component {
     state = {
         username: '',
@@ -8,6 +16,7 @@ class Form extends Component {
         email: ''
     } 
     render() { 
+        this.handleResize()
         return (
             <React.Fragment>
                 {this.signUp()} 
@@ -15,33 +24,40 @@ class Form extends Component {
         ); 
     }
 
+    handleResize = () => {
+        window.addEventListener('resize', () => {
+            console.log(1)
+        })
+    }
+
     signUp(){
         return (
-            <form action = '/canada'>
-                <section>
-                    <div id = 'container'>
-                        <h1>Sign Up</h1>
-                        <div className = 'input'>
-                            <label htmlFor="username">Username</label>
-                            <input id = "username" type = 'text'></input>
-                        </div>
-                        <div className = 'input'>
-                            <label htmlFor="username">Email</label>
-                            <input id = "username" type = 'email'></input>
-                        </div>
-                        <div className = 'input'>
-                            <label htmlFor="password">Password</label>
-                            <input id = "password" type = 'password'></input>
-                        </div>
-                        <div className = 'input'>
-                            <label htmlFor="re_password">Re-enter Password</label>
-                            <input id = "re_password" type = 'password'></input>
-                        </div>
-                        <div>
-                            <button></button>
-                        </div>
+            <form>
+                <div id = 'container'>
+                    <h1>Sign Up</h1>
+                    <div>
+                        {/* keep working on this */}
                     </div>
-                </section>
+                    <div className = 'input'>
+                        <label htmlFor="username">Username</label>
+                        <input id = "username" type = 'text'></input>
+                    </div>
+                    <div className = 'input'>
+                        <label htmlFor="username">Email</label>
+                        <input id = "username" type = 'email'></input>
+                    </div>
+                    <div className = 'input'>
+                        <label htmlFor="password">Password</label>
+                        <input id = "password" type = 'password'></input>
+                    </div>
+                    <div className = 'input'>
+                        <label htmlFor="re_password">Re-enter Password</label>
+                        <input id = "re_password" type = 'password'></input>
+                    </div>
+                    <div>
+                        <button></button>
+                    </div>
+                </div>
             </form>
         );
     }
